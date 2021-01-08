98e47610e1aa9089c6777837a614a00b446e493a
Sonano <mohammadayyub46@gmail.com>
2021-01-08 17:02:18 +0700

    Update Seeder

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tags')->insert(
+          [
+            'name' => 'Absurd'
+          ],
+          [
+            'name' => 'Hukum'
+          ],
+          [
+            'name' => 'Kartun'
+          ],
+          [
+            'name' => 'Kesehatan'
+          ],
+          [
+            'name' => 'Makanan'
+          ],
+          [
+            'name' => 'Pembunuhan'
+          ],
+          [
+            'name' => 'Penculikan'
+          ],
+          [
+            'name' => 'Pendidikan'
+          ],
+          [
+            'name' => 'Permainan'
+          ],
+          [
+            'name' => 'Selebriti'
+          ],
+          [
+            'name' => 'Teknologi'
+          ],
+        );
+    }
+}
