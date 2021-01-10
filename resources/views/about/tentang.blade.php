21f060b5d8c0a6e790fb1d487cfd5970f5e0755a
alfin022 <alfinmiftahudin@gmail.com>
2021-01-10 22:12:35 +0700

    penambahan
    add js,img,laman tentang dan artikel

---
+++ resources/views/about/tentang.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts/main')
+
+@section('title', 'Tentang')
+
+@section('container')
+<!-- content -->
+
+<div class="container-artikel container-fluid">
+<div class="article card-deck card-header" style="margin:2em 0">
+    <p>
+        web ini dibuat bertujuan untuk menyelesaikan tugas akhir mata kuliah pilihan web framework yang mana projek ini dipimpin oleh Alfian Yunianto(1710631170045) sebagai project leader.
+        adapun anggota kelompok yang bertugas dalam pembuatan tampilan web, database, maintenance dan sebagainya.
+    </p>
+</div>
+    <h2>nama kelompok</h2>
+    <ul>
+        <li>M Ayyub salahudin al ayubi</li>
+        <li>Alfin Miftahudin</li>
+        <li>M aziz Lasawali</li>
+        <li>Chaerul Hadad</li>
+        <li>Izhari</li>
+        <li>Reza</li>
+    </ul>
+
+</div>
+
+
+
+@endsection
