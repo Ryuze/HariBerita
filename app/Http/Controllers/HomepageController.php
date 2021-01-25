75a391e9b6bead8bbbc6ba0f78f2fc8574cec704
Ryuze <alfianyunianto74@gmail.com>
2021-01-25 13:30:52 +0700

    Create HomepageController.php

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    public function search(Request $request)
+    {
+        //izhari kerjain bagian ini
+    }
+
+    public function show($id)
+    {
+        //ini bagian ane, jangan ente sentuh
+    }
+}
