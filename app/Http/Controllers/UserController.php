d74286731f1a666229e650ce72e1e3e7b8b207ad
Ryuze <alfianyunianto74@gmail.com>
2021-01-27 12:00:48 +0700

    Create UserController.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        //bagian heru buat nampilin view create
+    }
+
+    public function store(Request $request)
+    {
+        //bagian heru buat nyimpen data kiriman dari view vreate
+    }
+
+    public function edit()
+    {
+        //bagian azis buat nampilin view edit
+    }
+
+    public function update(Request $request)
+    {
+        //bagian azis buat update data kiriman view edit
+    }
+}
