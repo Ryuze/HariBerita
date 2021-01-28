f1af1127a61e366561084ea37d0ded048cd89d39
Azis <azisLasawali@gmail.com>
2021-01-28 23:34:27 +0700

    tambah control update akun

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -16,13 +20,77 @@ class UserController extends Controller
         //bagian heru buat nyimpen data kiriman dari view vreate
     }
 
-    public function edit()
+    public function edit($id)
     {
-        //bagian azis buat nampilin view edit
+        $users = User::findOrFail($id);
+        return view('user.edit', compact('users'));
     }
 
     public function update(Request $request)
     {
-        //bagian azis buat update data kiriman view edit
+        $users = User::find(Auth::user()->id);
+
+        if ($users){
+            $validate = null;
+            if(Auth::user()->email === $request['email']){
+                $validate = $request->validate([
+                    'name' => 'required|min:1',
+                    'email' => 'required|email'
+                ]);
+
+            } else {
+                $validate = $request->validate([
+                    'name' => 'required|min:1',
+                    'email' => 'required|email|unique:users'
+                ]);   
+            }
+
+            if ($validate) { 
+                $users->name = $request['name'];
+                $users->email = $request['email'];
+
+                $users->save();
+
+                $request->session()->flash('Sukses', 'Profil Berhasil Di Update');
+                return redirect()->back();
+            } else {
+                return redirect()->back();
+            }
+        } else {
+            return redirect()->back();
+        }
+    }
+
+    public function passwordUpdate(Request $request){
+        $validate = $request->validate([
+            'oldPassword' => 'required|min:8',
+            'password' => 'required|min:8|confirmed'
+        ]);   
+
+        $users = User::find(Auth::user()->id);
+
+        if ($users){
+            if (Hash::check($request['oldPassword'], $users->password) && $validate) {
+                $users->password = Hash::make($request['password']);
+                
+                $users->save();
+
+                $request->session()->flash('Sukses', 'Password Berhasil Di Update');
+                return redirect()->back();
+            } else {
+                $request->session()->flash('Gagal', 'Password Gagal Di Update');
+                return redirect()->back();
+            }
+        }
+    }
+
+    public function passwordEdit($id){
+        $users = User::findOrFail($id);
+        return view('user.password', compact('users'));
+    }
+
+    public function profil($id){
+        $users = User::findOrFail($id);
+        return view('user.profil', compact('users'));
     }
 }
